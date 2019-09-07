dbea664d9de4241be280469c9b3589db094d046b
Rafael Jeferson <rafa.jefer@gmail.com>
2019-09-07 19:48:46 -0300

    Criado a conexao com banco de dados

--- config.php
+++ config.php
@@ -1,3 +1,27 @@
 <?php
 require 'environment.php';
 
+$config = array();
+
+if (ENVIRONMENT == 'development') {
+	define("BASE_URL", "http://localhost/cursos/php/poo/mvc/recriando_estrutura_mvc/");
+	$config['dbname'] = 'bonieky_classificados';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = '';
+} else {	
+	define("BASE_URL", "");
+	$config['dbname'] = '';
+	$config['host'] = '';
+	$config['dbuser'] = '';
+	$config['dbpass'] = '';
+}
+
+try {
+	global $db;
+	$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+} catch(PDOException $e) {
+	echo "Erro: ".$e->getMessage();
+	exit;
+}
+
